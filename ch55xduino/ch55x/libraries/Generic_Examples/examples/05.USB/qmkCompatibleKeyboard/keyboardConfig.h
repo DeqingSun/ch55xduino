f52633274a812c7bd022de0b0d282b93912148af
Deqing Sun <iamseer@gmail.com>
2023-06-21 12:37:09 -0400

    format code

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/qmkCompatibleKeyboard/keyboardConfig.h
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/qmkCompatibleKeyboard/keyboardConfig.h
@@ -3,6 +3,6 @@
 #define COLS_COUNT 3
 #define LAYER_COUNT 2
 
-//pretend we are BN003 to let via know we are compatible
+// pretend we are BN003 to let via know we are compatible
 #define USB_VID 0x4249
 #define USB_PID 0x4287
