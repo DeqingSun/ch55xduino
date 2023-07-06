18b3c76be9166c3d618d50aa97002c9e9105a468
Deqing Sun <iamseer@gmail.com>
2023-07-06 07:34:57 -0400

    fix error

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/PowerDeliveryCH549/src/pd.c
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/PowerDeliveryCH549/src/pd.c
@@ -21,7 +21,6 @@
 #define TL0_RECV_BIT1_UPPER_LIMIT (129-8)
 #define TL0_RECV_BIT1_LOWER_LIMIT 101
 #define TL0_SEND_START_VALUE 176
-#define TL0_SEND_BIT1_TOGGLE (211-2)
 #define TL0_SEND_BIT1_TOGGLE (211)
 #else
 #error "This only run for 32M clock"
