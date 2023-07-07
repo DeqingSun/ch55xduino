5716221f918b381572253046df323ffc762e7644
Deqing Sun <iamseer@gmail.com>
2023-07-06 22:25:59 -0400

    optimize code

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/PowerDeliveryCH549/src/pd.c
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/PowerDeliveryCH549/src/pd.c
@@ -359,12 +359,57 @@ void RcvBufDecode5B4B(){
 }
 
 void SndBufEncode4B5B(){
-  __data uint8_t i = SndDataCount;
-  do{
-    i--;
-    SndDataBuf[4+i*2+0] = Cvt4B5B[SndDataBuf[i] & 0x0f];
-    SndDataBuf[4+i*2+1] = Cvt4B5B[SndDataBuf[i] >> 4];
-  }while(i!=0);
+  __asm__(
+    "    mov dptr,#_SndDataCount                  \n"
+    "    movx a,@dptr                             \n"
+    "    mov r7,a                                 \n"
+
+    " ; r5:r6 is the write ptr SndDataBuf[4+2*(SndDataCount-1)] \n"
+    "    dec a                                    \n"
+    "    mov r5,a                ;read ptr offset \n"
+    "    add a,acc                                \n"
+    "    add a,#04                                \n"
+    "    mov r6,a                ;write ptr offset\n"
+    "    mov r4,#0                                \n"
+
+    "SndBufEncode4B5B_loop$:                      \n"
+    "    mov a,#(_SndDataBuf)                     \n"
+    "    add a,r5                                 \n"
+    "    mov dpl,a                                \n"
+    "    mov a,#(_SndDataBuf >> 8)                \n"
+    "    addc a,r4                     ;r4 keep 0 \n"
+    "    mov dph,a                                \n"
+    "    movx a,@dptr                             \n"
+    "    mov r3,a                  ;SndDataBuf[i] \n"
+
+    "    anl a,#0xF0                              \n"
+    "    swap a                                   \n"
+    "    mov dptr,#(_Cvt4B5B)                     \n"
+    "    movc a,@a+dptr                           \n"
+    "    mov r2,a   ; Cvt4B5B[SndDataBuf[i] >> 4] \n"
+    "    mov a,r3                                 \n"
+    "    anl a,#0x0F                              \n"
+    "    mov dptr,#(_Cvt4B5B)                     \n"
+    "    movc a,@a+dptr                           \n"
+    "    mov r1,a  ;Cvt4B5B[SndDataBuf[i] & 0x0f] \n"
+    "    dec r5                                   \n"
+
+    "    mov a,#(_SndDataBuf)                     \n"
+    "    add a,r6                                 \n"
+    "    mov dpl,a                                \n"
+    "    mov a,#(_SndDataBuf >> 8)                \n"
+    "    addc a,r4                     ;r4 keep 0 \n"
+    "    mov dph,a                                \n"
+    "    mov a,r1                                 \n"
+    "    movx @dptr,a                             \n"
+    "    inc dptr                                 \n"
+    "    mov a,r2                                 \n"
+    "    movx @dptr,a                             \n"
+    "    dec r6                                   \n"
+    "    dec r6                                   \n"
+
+    "    djnz r7,SndBufEncode4B5B_loop$           \n"
+  );
 }
 
 uint8_t RcvCheckSOP(){
