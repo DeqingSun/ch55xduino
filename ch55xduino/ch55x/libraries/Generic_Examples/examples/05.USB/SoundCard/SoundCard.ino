0e23a5a1d9dc4382e02e4122f39284fe489a2c22
Deqing Sun <iamseer@gmail.com>
2024-03-29 10:22:05 -0400

    add comment

--- ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/SoundCard/SoundCard.ino
+++ ch55xduino/ch55x/libraries/Generic_Examples/examples/05.USB/SoundCard/SoundCard.ino
@@ -1,3 +1,20 @@
+/*
+  USB sound card example
+
+  created 2024
+  by Deqing Sun for use with CH55xduino
+
+  CH552 act as mono 24000K sound card
+  playback high 8bit on P3.4
+
+  Improved from ziv2013's work
+
+  This example code is in the public domain.
+
+  cli board options: usb_settings=user148
+
+*/
+
 #ifndef USER_USB_RAM
 #error "This example needs to be compiled with a USER USB setting"
 #endif
